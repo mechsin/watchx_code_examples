79a7fb3524af9fd119c8bf41db66703a0eac0855
Christopher Nyland <menyland@gmail.com>
2021-08-22 21:24:10 -0400

    Motion Activate light LED on detect
    Add some code to light the LED for 2 seconds when motion
    above the thresholds is detected. Additionally I am
    cleaning up some logging now that I have everything figured
    out.

--- motion_activated/motion_activated.ino
+++ motion_activated/motion_activated.ino
@@ -1,17 +1,21 @@
+// Simple example of how to turn on the LED
+// When motion in a certian range is detected
+// by the accelerometer.
 
 #include <Wire.h>
 #include <MPU6050.h>
 
 MPU6050 mpu;
 Vector reading;
+unsigned long activateDuration, activateTime = millis();
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
 
   Serial.println("Initialize MPU6050");
 
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G, 0x69))
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G, 0x69))
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
@@ -19,18 +23,23 @@ void setup()
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  reading = mpu.readScaledAccel();
-  if (0.3 < reading.XAxis && reading.XAxis < 0.8) {
-    digitalWrite(13, HIGH);
-  } else {
+
+  activateDuration = millis() - activateTime;
+  if (activateDuration > 2000) {
     digitalWrite(13, LOW);
   }
-//  Serial.print(reading.XAxis);
-//  Serial.print(",");
-//  Serial.print(reading.YAxis);
-//  Serial.print(",");
-//  Serial.println(reading.ZAxis);
-//  
-//  delay(1000);
+
+  reading = mpu.readScaledAccel();
+  if (0.3 < reading.XAxis && reading.XAxis < 0.8) {
+    activateTime = millis();
+    digitalWrite(13, HIGH);
+  }
+
+  //  Serial.print(reading.XAxis);
+  //  Serial.print(",");
+  //  Serial.print(reading.YAxis);
+  //  Serial.print(",");
+  //  Serial.println(reading.ZAxis);
+  //
+  //  delay(1000);
 }
